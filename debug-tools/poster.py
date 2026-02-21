7a510f4238b2ff4cc892245c845acf99ddbbf65c
CrashLogger <erlantz.alonso@live.com>
2026-02-21 02:23:37 +0100

    tools now use .env giles
    security 100

--- debug-tools/poster.py
+++ debug-tools/poster.py
@@ -1,11 +1,17 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-api_url = "http://127.0.0.1:5000/discordbotapi"
+api_url = "https://app.galerna.eus/discordbotapi"
 
-file_path = "debug-tools/patata.png"
+file_path = "debug-tools/angery-admin.png"
+
+load_dotenv()
+username = os.getenv("REAL_API_LOGIN")
+hash = os.getenv("REAL_API_HASH")
 
 with open(file_path, "rb") as f:
-    data = {"username": "test", "password_hash": "098f6bcd4621d373cade4e832627b4f6"}
+    data = {"username": username, "password_hash": hash}
     files = {"file": (file_path.split('/')[-1], f)}
     response = requests.post(api_url, data=data, files=files)
 
