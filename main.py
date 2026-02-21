ea4625d2f3b0c4814ebd3b4410f7204dfd29509a
pmartine082 <pmartinez082@ikasle.ehu.eus>
2026-02-21 22:23:47 +0100

    konpondu bug (api localhost + threading-a kenduta)

--- main.py
+++ main.py
@@ -4,14 +4,5 @@ from controller.bot import run_bot
 
 if __name__ == '__main__':
    
-   
-
-    # Start Flask API in a separate thread
-    
-    api_thread = Thread(target=run_api)
-    api_thread.daemon = True
-    api_thread.start()
-
-    # Start Discord bot in the main thread
-    run_bot()
+    run_api()
     
