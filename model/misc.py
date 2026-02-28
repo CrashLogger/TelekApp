bd2fc7a409f2703098395f1ce7334c3cf54dd83e
CrashLogger <erlantz.alonso@live.com>
2026-02-28 18:08:02 +0100

    Version bump

--- model/misc.py
+++ model/misc.py
@@ -1,6 +1,6 @@
 # Cosas que no importan así que van al repo
 
-VERSION="b-2026.02d"
+VERSION="b-2026.02e"
 
 URL="https://app.galerna.eus/TelekApp/"
 ISSUES="https://github.com/CrashLogger/TelekApp/issues"
