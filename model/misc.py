5d863826f4d94b4331cd2ac63a683a0d4edff807
CrashLogger <erlantz.alonso@live.com>
2026-03-11 08:14:09 +0100

    Bumped version number for #30
    Bumped version number

--- model/misc.py
+++ model/misc.py
@@ -1,6 +1,6 @@
 # Cosas que no importan así que van al repo
 
-VERSION="2026.03b"
+VERSION="2026.03c"
 
 URL="https://app.galerna.eus/TelekApp/"
 ISSUES="https://github.com/CrashLogger/TelekApp/issues"
